8413a0888e809e412e0c4a94cfd813a8995a869d
Liza-Aleks1108 <85190880+Liza-Aleks1108@users.noreply.github.com>
2024-03-16 16:43:58 +0200

    Accordion

--- js/lesson-04/js/modul-8.js
+++ js/lesson-04/js/modul-8.js
@@ -4,3 +4,11 @@
  * при кліку на елемент меню, розкривай блок з текстом.При повторному кліку по елементу,
  * розкритий текст приховується.Нажимаючи на інші елементи меню, попередньо відкриті елементи не закриваються.
  */
+
+const listEl = document.querySelector('.js-accordion-list');
+
+listEl.addEventListener('click', event => {
+  const target = event.target;
+  if (target.nodeName !== 'BUTTON') return;
+  target.nextElementSibling.classList.toggle('active');
+});
