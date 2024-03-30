1e5c45ba9a4f1f798b8c997660dfa395c9519776
kulikp1 <pashakylik2@gmail.com>
2024-03-30 17:25:37 +0200

    Module 10 do task 1, 2, 4

---
+++ js/lesson-05/module-10/js/calendar.js
@@ -0,0 +1,13 @@
+// import { month, day, weekDay, year } from "./date.js";
+// const display = document.querySelector(".js-container");
+// document.addEventListener('DOMContentLoaded', renderPage)
+
+
+// function renderPage() {
+//     display.querySelector(".js-month").textContent = month;
+//     display.querySelector(".js-day-number").textContent = day;
+//     display.querySelector(".js-day").textContent = weekDay;
+//     display.querySelector(".js-year").textContent = year;
+// }
+
+
---
+++ js/lesson-05/module-10/js/date.js
@@ -0,0 +1,5 @@
+// const currentDate = new Date();
+// export const month = currentDate.toLocaleDateString('uk-UA', { month: 'long' });
+// export const year = currentDate.toLocaleDateString('uk-UA', { year: 'numeric' });
+// export const weekDay = currentDate.toLocaleDateString('uk-UA', { weekday: 'long' });
+// export const day = currentDate.toLocaleDateString('uk-UA', { day: 'numeric' });
